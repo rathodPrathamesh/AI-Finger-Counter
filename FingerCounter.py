be2e1521099fe4fffda2e703076dfccbd55d083c
Prathamesh Rathod <88021199+rathodPrathamesh@users.noreply.github.com>
2023-06-30 00:09:56 +0530

    Update FingerCounter.py

--- FingerCounter.py
+++ FingerCounter.py
@@ -1,7 +1,6 @@
 import cv2
 import time
 import HandTrackingModule as htm
-# import os
 
 wCam, hCam = 720,670
 pTime=0
@@ -10,18 +9,6 @@ cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 totalFingers =0
-
-# mylist = os.listdir("Fingers")
-# mylist.sort()
-# mylist.pop(0)
-# #print(mylist)
-
-
-# overlaylist = []
-# for imPath in mylist:
-#     image = cv2.imread(f'Fingers/{imPath}')
-#     overlaylist.append(image)
-
 
 
 detector = htm.handDetector(detection_confidence = 0.8)
@@ -58,18 +45,15 @@ while True:
 
         totalFingers = fingers.count(1)
 
+        cv2.rectangle(img, (20, 300), (120, 420), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, f'{int(totalFingers)}', (40, 390), cv2.FONT_HERSHEY_PLAIN, 6, (0, 100, 0), 10)
 
-        # h, w, c = overlaylist[totalFingers-1].shape
-        # img[0:h,0:w] = overlaylist[totalFingers-1]
-        cv2.rectangle(img, (20, 455), (170, 625), (0, 255, 0), cv2.FILLED)
-        cv2.putText(img, f'{int(totalFingers)}', (70, 565), cv2.FONT_HERSHEY_PLAIN, 6, (0, 100, 0), 10)
-
+    
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime=cTime
 
-    cv2.putText(img, f'FPS: {int(fps)}', (900,70), cv2.FONT_HERSHEY_PLAIN, 2, (0,100,0), 4)
-
+    cv2.putText(img, f'FPS: {int(fps)}', (500,50), cv2.FONT_HERSHEY_PLAIN, 2, (0,100,0), 4)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
